8b1d65b169a16034fc9b95bec12cc806ff5a32dc
github-action-benchmark <github@users.noreply.github.com>
2023-03-04 14:42:51 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for d84d356c5153d96cb24c50243a2fe08162c616bd

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1677940971461,
+  "repoUrl": "https://github.com/lechu445/Microsoft.IO.RecyclableMemoryStream",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "xxx@example.com",
+            "name": "lechu445"
+          },
+          "committer": {
+            "email": "xxx@example.com",
+            "name": "lechu445"
+          },
+          "distinct": true,
+          "id": "d84d356c5153d96cb24c50243a2fe08162c616bd",
+          "message": "Merge branch 'continous-benchmarking'",
+          "timestamp": "2023-03-04T15:41:25+01:00",
+          "tree_id": "1328148e1c2589d77f675c135d695c6978f1fa1a",
+          "url": "https://github.com/lechu445/Microsoft.IO.RecyclableMemoryStream/commit/d84d356c5153d96cb24c50243a2fe08162c616bd"
+        },
+        "date": 1677940969889,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BenchmarkTests.WriteTest.WriteByte",
+            "value": 71175205.93333334,
+            "unit": "ns",
+            "range": "± 251265.2462130313"
+          },
+          {
+            "name": "BenchmarkTests.WriteTest.WriteSpan",
+            "value": 195117907.22727272,
+            "unit": "ns",
+            "range": "± 7250284.958323425"
+          }
+        ]
+      }
+    ]
+  }
+}
